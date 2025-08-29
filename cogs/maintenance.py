714012aaccb60720117c5bf1c3c7725d92d69e45
Dylan <dkooby1@gmail.com>
2025-08-29 11:49:07 -0400

    Added sync and unsync commands, need to fix owner checks and the command prefix but other than that, they work

--- cogs/maintenance.py
+++ cogs/maintenance.py
@@ -1,56 +1,66 @@
 import discord
 from discord import app_commands
+from discord.app_commands import check
 from discord.ext import commands
-from discord.ext.commands import Context
+from __main__ import system_embed_hex
 
-class Setup(commands.Cog, name="setup"):
-    def __init__(self, bot) -> None:
+def is_owner(interaction: discord.Interaction) -> bool:
+    return interaction.user.id == 257159805070868481
+
+class Maintenance(commands.Cog, name="maintenance"):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @discord.app_commands.command(
+    @commands.hybrid_command(
         name="sync",
-        description="Syncs slash commands.",
+        description="Syncs all bot slash commands",
     )
-    @app_commands.describe(scope="The sync command's scope, can be 'global' or 'guild'")
-    @commands.is_owner()
-    async def sync(self, context: Context, scope: str) -> None:
+    @discord.app_commands.describe(scope="The sync command's scope, can be 'global' or 'guild'")
+    @discord.app_commands.check(is_owner)
+    async def sync(self, context: commands.Context, scope: str) -> None:
         if scope == "global":
             await context.bot.tree.sync()
             embed = discord.Embed(
-                description="Slash commands have been synced globally.",
-                color=0xBEBEFE,
+                description="Slash commands have been synced globally",
+                color=int(system_embed_hex, 16),
             )
             await context.send(embed=embed)
             return
         elif scope == "guild":
+            context.bot.tree.clear_commands(guild=context.guild)
             context.bot.tree.copy_global_to(guild=context.guild)
             await context.bot.tree.sync(guild=context.guild)
             embed = discord.Embed(
-                description="Slash commands have been synced in this guild.",
-                color=0xBEBEFE,
+                description="Slash commands have been synced in this guild only",
+                color=int(system_embed_hex, 16),
             )
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description="The scope must be `global` or `guild`.", color=0xE02B2B
+            description="The scope must be `global` or `guild`.",
+            color=int(system_embed_hex, 16),
         )
         await context.send(embed=embed)
 
-    @discord.app_commands.command(
+    @commands.hybrid_command(
         name="unsync",
-        description="Unsyncs slash commands.",
+        description="Unsyncs all bot slash commands",
     )
-    @app_commands.describe(
-        scope="The sync command's scope, can be 'global' or 'guild'"
-    )
-    @commands.is_owner()
-    async def unsync(self, context: Context, scope: str) -> None:
+    @discord.app_commands.describe(scope="The unsync command's scope, can be 'global' or 'guild'")
+    @discord.app_commands.check(is_owner)
+    async def unsync(self, context: commands.Context, scope: str) -> None:
         if scope == "global":
-            context.bot.tree.clear_commands(guild=None)
-            await context.bot.tree.sync()
+            for cmd in list(context.bot.tree.get_commands(guild=context.guild)):
+                if cmd.cog_name != "Maintenance":
+                    context.bot.tree.remove_command(cmd.name, guild=context.guild)
+            try:
+                await context.bot.tree.sync(guild=context.guild)
+
+            except Exception as e:
+                context.bot.logger.error(f"Something went wrong: {type(e).__name__}: {e}")
             embed = discord.Embed(
-                description="Slash commands have been unsynced globally.",
-                color=0xBEBEFE,
+                description="Slash commands have been unsynced globally",
+                color=int(system_embed_hex, 16),
             )
             await context.send(embed=embed)
             return
@@ -58,25 +68,16 @@ class Setup(commands.Cog, name="setup"):
             context.bot.tree.clear_commands(guild=context.guild)
             await context.bot.tree.sync(guild=context.guild)
             embed = discord.Embed(
-                description="Slash commands have been unsynced in this guild.",
-                color=0xBEBEFE,
+                description="Slash commands have been unsynced in this guild only",
+                color=int(system_embed_hex, 16),
             )
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description="The scope must be `global` or `guild`.", color=0xE02B2B
+            description="The scope must be `global` or `guild`.",
+            color=int(system_embed_hex, 16),
         )
         await context.send(embed=embed)
 
-    @discord.app_commands.command(
-        name="shutdown",
-        description="Make the bot shutdown.",
-    )
-    @commands.is_owner()
-    async def shutdown(self, context: Context) -> None:
-        embed = discord.Embed(description="Shutting down...", color=0xBEBEFE)
-        await context.send(embed=embed)
-        await self.bot.close()
-
 async def setup(bot) -> None:
-    await bot.add_cog(Setup(bot))
+    await bot.add_cog(Maintenance(bot))
